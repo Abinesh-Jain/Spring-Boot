0459488c978d5347104dedbf542e9571cbfcaa05
Abinesh Jain <amsiabinesh@gmail.com>
2023-01-02 11:18:35 +0530

    My First Spring Project

---
+++ demo/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class Student {
+
+		int studid=1;
+		String studname="Abi";
+		void getdetails()
+		{
+			System.out.print("Student name and id is "+studname+" and "+studid);
+		}
+	
+}
